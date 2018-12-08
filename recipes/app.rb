46e08fa04305df6f142dfe7f21ede28c136f406f
Kalin Staykov <k.t.staykov@gmail.com>
2018-12-08 18:12:04 +0200

    update recipe to install app requirements

--- recipes/app.rb
+++ recipes/app.rb
@@ -11,3 +11,10 @@ git '/home/vagrant/django-realworld-example-app' do
     revision 'master'
     action :sync
 end
+
+execute "pip-install-requirements" do
+    user 'vagrant'
+    group 'vagrant'
+    command ". /home/vagrant/.bash_profile && pyenv activate productionready && cd /home/vagrant/django-realworld-example-app && pip install -r requirements.txt"
+    action :run
+end
