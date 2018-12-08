4e203c2d73bdc767ae0a7b167633ec2d0ee514f0
Kalin Staykov <k.t.staykov@gmail.com>
2018-12-08 17:14:24 +0200

    converge python related actions in single recipe

--- recipes/prepare_python.rb
+++ recipes/prepare_python.rb
@@ -4,6 +4,12 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+git '/home/vagrant/.pyenv/plugins/pyenv-virtualenv' do
+    repository 'https://github.com/pyenv/pyenv-virtualenv.git'
+    revision 'master'
+    action :sync
+end
+
 cookbook_file '/home/vagrant/.bash_profile' do
     source 'bash_profile'
     owner 'vagrant'
@@ -26,4 +32,18 @@ execute "pyenv-install-3.5.2" do
     not_if { ::File.exist?('/home/vagrant/.pyenv/versions/3.5.2') }
     command ". /home/vagrant/.bash_profile && pyenv install 3.5.2"
     action :run
+end
+
+execute "pyenv-install-3.5.2" do
+    user 'vagrant'
+    group 'vagrant'
+    not_if { ::File.exist?('/home/vagrant/.pyenv/versions/productionready') }
+    command ". /home/vagrant/.bash_profile && pyenv virtualenv 3.5.2 productionready"
+    action :run
+end
+
+file '/home/vagrant/.python-version' do
+    user 'vagrant'
+    group 'vagrant'
+    content 'productionready'
 end
